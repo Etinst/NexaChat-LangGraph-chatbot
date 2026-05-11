d232c167ec59f4506b19bae64bd82e011396f19b
Darshan Nitin Barhate <146816807+Etinst@users.noreply.github.com>
2026-05-11 10:05:53 +0530

    Delete nexachat directory

--- nexachat/backend/__init__.py
+++
@@ -1,4 +0,0 @@
-"""NexaChat backend package."""
-from .graph import chat, stream_chat, get_thread_history, DEFAULT_SYSTEM_PROMPT
-
-__all__ = ["chat", "stream_chat", "get_thread_history", "DEFAULT_SYSTEM_PROMPT"]
